2f684c777de452ac8f533d5dd9e2152da9cc1a44
vadimcola <vadimcola@mail.ru>
2023-12-22 17:11:57 +0700

    Реализована модель Supplier

--- network/models.py
+++ network/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Supplier(models.Model):
+
+    CATEGORY = (
+        (0, 'Factory'),
+        (1, 'Retail Network'),
+        (2, 'Individual Entrepreneur')
+    )
+
+    name = models.CharField(max_length=255, verbose_name='Наименование поставщика')
+    category = models.IntegerField(choices=CATEGORY, verbose_name='Категория поставщика')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Поставщик'
+        verbose_name_plural = 'Поставщики'
+
+
