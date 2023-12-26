d1e5975540752caa2440b7b41e91c99d96480758
vadimcola <vadimcola@mail.ru>
2023-12-26 15:33:27 +0700

    Сделаны настройки админ панели.

--- network/admin.py
+++ network/admin.py
@@ -20,4 +20,18 @@ class SupplierAdmin(admin.ModelAdmin):
 
 @admin.register(Network)
 class NetworkAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    list_display = ('name', 'category', 'supplier', 'debt')
+    list_filter = ('contact__city',)
+    actions = ['clear_debt']
+
+    def get_supplier(self, obj):
+        return obj.supplier.name
+    get_supplier.admin_order_field = 'supplier__name'
+
+    def clear_debt(self, request, queryset):
+        for obj in queryset:
+            obj.debt = 0
+            obj.save()
+
+    clear_debt.short_description = 'Очистить задолженность перед поставщиком'
+
