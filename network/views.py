90192b71b287965288feb4ad0eb2a0111ab10a4c
vadimcola <vadimcola@mail.ru>
2023-12-27 10:52:44 +0700

    Реализован CRUD для всех моделей.

--- network/views.py
+++ network/views.py
@@ -1,7 +1,23 @@
 from rest_framework import viewsets
 
-from network.models import Network
-from network.serializers import NetworkSerializer, ViewNetworkSerializer
+from network.models import Network, Contact, Product, Supplier
+from network.serializers import NetworkSerializer, ViewNetworkSerializer, ContactSerializer, ProductSerializer, \
+    SupplierSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class SupplierViewSet(viewsets.ModelViewSet):
+    queryset = Supplier.objects.all()
+    serializer_class = SupplierSerializer
 
 
 class NetworkViewSet(viewsets.ModelViewSet):
